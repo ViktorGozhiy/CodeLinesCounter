202c6c77d6d25e957a343583a9fd248eef523697
Viktor <viktor.gozhiy@outlook.com>
2020-10-07 17:29:50 +0300

    Init commit

---
+++ src/main/java/com/github/viktorgozhiy/clcounter/LinesCounter.java
@@ -0,0 +1,10 @@
+package com.github.viktorgozhiy.clcounter;
+
+import java.io.IOException;
+
+public class LinesCounter {
+
+    public static int countLines(String filePath) throws IOException {
+        return 0;
+    }
+}
